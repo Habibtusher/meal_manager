2de72394d921e83a8b16ab157139ce99a2959860
habibur <habibur@mediusware.com>
2026-01-05 17:18:47 +0600

    feat: Add responsive sidebar navigation, PWA support, initial homepage, and Netlify deployment configuration.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = 'meal-manager-v1';
+const CACHE_NAME = 'meal-manager-v2';
 const urlsToCache = [
-    '/',
     '/manifest.json',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
@@ -8,15 +7,17 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open(CACHE_NAME)
         .then((cache) => {
-        return cache.addAll(urlsToCache);
-    }));
+            return cache.addAll(urlsToCache);
+        }));
 });
 self.addEventListener('fetch', (event) => {
     event.respondWith(caches.match(event.request)
         .then((response) => {
-        if (response) {
-            return response;
-        }
-        return fetch(event.request);
-    }));
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        }).catch(() => {
+            return fetch(event.request);
+        }));
 });
